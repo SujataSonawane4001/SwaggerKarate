d9a65fa33413a1397fc25e5555cc33acc2e7a4f7
SujataSonawane4001 <sujata62sonawane@gmail.com>
2025-03-04 12:59:23 +0530

    First Push of karate

---
+++ src/main/java/com/karate1/sujata.java
@@ -0,0 +1,7 @@
+package com.karate1;
+
+public class sujata {
+public static void main(String[] args) {
+System.out.println("hii");	
+}
+}
---
+++ src/test/java/chaitrnya.java
@@ -0,0 +1,6 @@
+
+public class chaitrnya {
+	public static void main(String[] args) {
+		System.out.println("hii");	
+		}
+}
